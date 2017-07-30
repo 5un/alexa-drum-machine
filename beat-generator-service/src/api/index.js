5abf61265a9844f920acfb6dbf94fdcb427ba63d
Soravis Prakkamakul <asunnotthesun@gmail.com>
2017-07-29 22:02:36 -0700

    Added song generation endpoi endpoint

--- beat-generator-service/src/api/index.js
+++ beat-generator-service/src/api/index.js
@@ -3,12 +3,18 @@ import { Router } from 'express';
 import facets from './facets';
 import fs from 'fs';
 import uuidv1 from 'uuid/v1';
+import FuzzySet from 'fuzzyset.js'
 import { generateGroove } from '../lib/mma';
 import config from '../config.json';
 import secrets from '../../secrets/aws.json';
-
+import songList from '../lib/songs';
 var exec = require('child_process').exec;
 import AWS from 'aws-sdk';
+import _ from 'lodash';
+
+const songDatabase = FuzzySet(songList);
+const songCache = {};
+
 var accessKeyId =  process.env.AWS_ACCESS_KEY || secrets.AWS_ACCESS_KEY;
 var secretAccessKey = process.env.AWS_SECRET_KEY || secrets.AWS_SECRET_KEY;
 
@@ -18,6 +24,42 @@ AWS.config.update({
 });
 
 var s3 = new AWS.S3();
+
+const generateTrack = (mma, filename, cb) => {
+  fs.writeFile(`./temp/${filename}.mma`, mma, function(err) {
+    if(err) {
+      cb({ message: 'Cannot generate MMA file' }, null);
+    } else {
+      const script = `mma ./temp/${filename}.mma &&
+        fluidsynth ./soundfonts/FluidR3_GM.sf2 ./temp/${filename}.mid -F ./temp/${filename}.wav &&
+        lame --scale 5 ./temp/${filename}.wav ./temp/${filename}.mp3`
+      let child;
+      child = exec(script, function (error, stdout, stderr) {
+        if (error !== null) {
+          cb({ message: `Cannot execute command ${error}` }, null);
+        } else {
+          fs.readFile(`./temp/${filename}.mp3`, function(err, file_buffer){
+            var params = {
+              Bucket: config.s3Bucket,
+              Key: `${config.s3Path}/${filename}.mp3`,
+              Body: file_buffer,
+              ACL:'public-read'
+            };
+
+            s3.putObject(params, function (perr, pres) {
+              if (perr) {
+                cb({ message: 'Cannot upload file', s3Error: perr }, null);
+              } else {
+                cb(null, { url: `https://s3.amazonaws.com/${config.s3Bucket}/${config.s3Path}/${filename}.mp3` });
+              }
+            });
+        });
+                  
+        }
+      });
+    }
+  }); 
+};
 
 export default ({ config, db }) => {
 	let api = Router();
@@ -35,39 +77,45 @@ export default ({ config, db }) => {
     const tempo = req.query.tempo || 100;
     const groove = req.query.groove || 'pop';
     const mma = generateGroove({ tempo: tempo, groove: groove });
-    fs.writeFile(`./temp/${filename}.mma`, mma, function(err) {
+    generateTrack(mma, filename, (err, gres) => {
       if(err) {
-        res.json({ error: { message: 'Cannot generate MMA file' } });
+        res.status(400).send({ error: err });
       } else {
-        const script = `mma ./temp/${filename}.mma &&
-          fluidsynth ./soundfonts/FluidR3_GM.sf2 ./temp/${filename}.mid -F ./temp/${filename}.wav &&
-          lame --scale 5 ./temp/${filename}.wav ./temp/${filename}.mp3`
-        let child;
-        child = exec(script, function (error, stdout, stderr) {
-          if (error !== null) {
-            res.json({ error: { message: `Cannot execute command ${error}` } });
-          } else {
-            fs.readFile(`./temp/${filename}.mp3`, function(err, file_buffer){
-              var params = {
-                Bucket: config.s3Bucket,
-                Key: `${config.s3Path}/${filename}.mp3`,
-                Body: file_buffer,
-                ACL:'public-read'
-              };
-
-              s3.putObject(params, function (perr, pres) {
-                if (perr) {
-                  res.json({ error: { message: 'Cannot upload file', s3Error: perr } });
-                } else {
-                  res.json({ url: `https://s3.amazonaws.com/${config.s3Bucket}/${config.s3Path}/${filename}.mp3` });
-                }
-              });
-          });
-                    
-          }
-        });
+        res.json(gres);
       }
-    }); 
+    });
+  });
+
+  api.get('/songs/search', (req, res) => {
+    const query = req.query.q || '';
+    // Find first match
+    const matches = _.filter(
+                      _.map(
+                        songDatabase.get(query), (item) => ({ confidence: item[0], name: item[1] })
+                      ),
+                      (item) => (item.confidence > 0.7)
+                    );
+    if(matches.length > 0) {
+      const song = matches[0];
+      const filename = uuidv1();
+      fs.readFile(`./songs/${song.name}.mma`, function (err, mma) {
+        if (err) {
+          res.status(400).send({ error: { message: 'Cannot read song file' } });
+        } else {
+          generateTrack(mma, filename, (err, gres) => {
+            if(err) {
+              res.status(400).send({ error: err });
+            } else {
+              res.json(Object.assign(gres, song));
+            }
+          });
+        }
+        
+      });
+      
+    } else {
+      res.status(400).json({ error: { message: 'No match' } });
+    }
   });
 
 	return api;
